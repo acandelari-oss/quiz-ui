938a9444cbee185d02d625aa6bae8f37a6b9061b
acandelari-oss <acandelari@gmail.com>
2026-03-02 19:40:59 +0100

    Fix import statement formatting in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-
+import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
 
 return (
