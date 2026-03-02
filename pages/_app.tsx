c64c03063e844dc9400978fbc8cfef48643db195
acandelari-oss <acandelari@gmail.com>
2026-03-02 19:46:28 +0100

    Refactor App component to simplify Head usage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,19 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import type { AppProps } from 'next/app'
+
 export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
 
-return (
-
-<>
-
-<Head>
-
-<link
-
-href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap"
-
-rel="stylesheet"
-
-/>
-
-</Head>
-
-<div style={{ fontFamily:"Nunito, sans-serif" }}>
-
-<Component {...pageProps} />
-
-</div>
-
-</>
-
-);
-
+      <div style={{ fontFamily: "Nunito, sans-serif" }}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
